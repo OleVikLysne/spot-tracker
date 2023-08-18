573e1fbbb639efb1a65d04258c6e81945bc5f93a
Ole Vik Lysne <oleviklysne@gmail.com>
2023-08-18 10:05:15 +0200

    working solution

--- spot_track.py
+++ spot_track.py
@@ -1,117 +1,270 @@
 from rich.table import Table
 from rich.console import Console
-from configparser import ConfigParser
-import json
-from utils import average, stdev, load_json
+from utils import average, stdev, load_json, dump_json, get_current_season_id, get_first_season_id, update_config
+from enum import Enum
+import re
+
+QUERY_TYPE = Enum("QUERY_TYPES", ["ADD", "REVERT", "AVERAGE", "FIRST_X", "LAST_X", "SEASON_X", "NEW_SEASON", "TRACK_DATA", "DIST", "HELP"])
+TRACK_DATA_PATH = "backup.json"
+TRACKS: dict = load_json("tracks.json")
 
 
-
-def get_current_season() -> int:
-    config = ConfigParser()
-    config.read("settings.ini")
-    return int(config["GENERAL"]["currentseason"])
-
-def get_first_season() -> int:
-    config = ConfigParser()
-    config.read("settings.ini")
-    return int(config["GENERAL"]["firstseason"])
-
-
-
-
-# def fetch_season_raw(id: int) -> str:
-#     with open(f"season{id}.txt", "r") as f:
-#         return f.read()
-
-
-# def build_df(rawdata: str) -> dict:
-#     df = {}
-#     data = rawdata.split("\n")
-#     for track_data in data:
-#         track_data = track_data.split(" ")
-#         track_name = track_data[0]
-#         track_name = track_name[0:-1] # removes the colon
-#         track_scores = track_data[1:]
-#         if len(track_name) > 0 and len(track_scores) > 0:
-#             df[track_name] = [int(score) for score in track_scores]
-
-#     return df
-
-# def fetch_season(id) -> dict:
-#     rawdata = fetch_season_raw(id)
-#     return build_df(rawdata)
-
-# def fetch_seasons(*args) -> dict:
-#     return merge_seasons(*(fetch_season(id) for id in args))
-
-# def merge_seasons(*args):
-#     df = args[0]
-#     for season in args[1:]:
-#         for track in df.keys():
-#             if track in season:
-#                 df[track] += season[track]
-
-#     return df
-
-def get_display_format(data):
-    sorted_data_array = []
-    for track, scores in data.items(): # Build an array containing data for console display
-        avg = round(average(scores), 1)
-        race_count = len(scores)
-        std = round(stdev(scores), 1)
-        sorted_data_array.append((avg, track, race_count, std))
-
-    sorted_data_array.sort(reverse=True)
-    return sorted_data_array
-
-def get_general_info(data) -> tuple:
-    sum = 0
-    races = 0
-    for scores in data.values():
-        for score in scores:
-            sum += score
-            races += 1
+def get_query_type(query: str) -> QUERY_TYPE:
+    if query == "avg" or query == "average":
+        return QUERY_TYPE.AVERAGE
+    if query == "new season":
+        return QUERY_TYPE.NEW_SEASON
+    if query == "dist" or query == "distribution":
+        return QUERY_TYPE.DIST
+    if query == "help":
+        return QUERY_TYPE.HELP
     
-    overall_avg = round(sum*12/races, 1)
-    total_mogis = int(round(races/12, 0))
+    args = query.split()
+    if len(args) == 2 and args[0] in TRACKS.keys() and args[1].isnumeric():
+        return QUERY_TYPE.ADD
+    if len(args) == 2 and args[0] in TRACKS.keys() and args[1] == "revert":
+        return QUERY_TYPE.REVERT
+    if len(args) == 2 and args[0] in TRACKS.keys() and args[1] == "data":
+        return QUERY_TYPE.TRACK_DATA
+    if len(args) == 2 and args[0] == "last" and args[1].isnumeric():
+        return QUERY_TYPE.LAST_X
+    if len(args) == 2 and args[0] == "first" and args[1].isnumeric():
+        return QUERY_TYPE.FIRST_X
+    if (
+        (query == "cur" or query == "current") or
+        (len(args) == 2 and args[0] == "season" and args[1].isnumeric()) or
+        (len(args) == 1 and query[0] == "s" and query[1:].isnumeric())
+    ):
+        return QUERY_TYPE.SEASON_X
+    return None
+
+
+def get_seasons(*season_ids: tuple[int]) -> dict:
+    TRACK_DATA: dict = load_json(TRACK_DATA_PATH)
+    master_dict = {}
+    for id in season_ids:
+        season = f"season_{id}"
+        if season not in TRACK_DATA:
+            continue
+        season_data = TRACK_DATA[season]
+
+        for track, scores in season_data.items():
+            if track not in TRACKS.keys():
+                raise ValueError("Invalid track name.")
+            if track in master_dict:
+                master_dict[track] += scores
+            else:
+                master_dict[track] = scores
+    return master_dict
+
+
+def trim_to_subset(df: dict, amount: int, side="back") -> None:
+    for track, race_scores in df.items():
+        if len(race_scores) <= amount:
+            continue
+        if side == "back":
+            df[track] = race_scores[-amount:]
+        elif side == "front":
+            df[track] = race_scores[:amount]
+        else:
+            raise ValueError("Invalid side selection")
+
+
+def get_macro_info(df: dict) -> tuple:
+    races_played = sum(len(x) for x in df.values())
+    total_pts_scored = sum(sum(df.values(), []))
+    overall_avg = round(total_pts_scored*12/races_played, 1)
+    total_mogis = int(round(races_played/12, 0))
     return overall_avg, total_mogis
 
 
-
-def print_table(data, title=""):
+def display_table(
+        data_array: list[tuple], 
+        *columns: tuple[str], 
+        title: str = None, 
+        styles: list[str] = None,
+        justify: list[str] = None
+        ):
+    styles = ["cyan", "green", "white", "red"] if styles is None else styles
+    justify = ["center" for _ in range(len(columns))] if justify is None else justify
     table = Table(title=title)
-    table.add_column("avg", style="cyan")
-    table.add_column("track", style="magenta")
-    table.add_column("races", style="green")
-    table.add_column("STDev", style="red")
-
-    for track_data in data:
+    for i, column in enumerate(columns):
+        table.add_column(column, style=styles[i%len(styles)], justify=justify[i])
+    for track_data in data_array:
         table.add_row(*(str(x) for x in track_data))
+    Console().print(table)
+
+
+def add_race(track: str, pts: int, season_id: int = None) -> None:
+    season_id = get_current_season_id() if season_id is None else season_id
+    track_data = load_json(TRACK_DATA_PATH)
+    season = f"season_{season_id}"
+    if season not in track_data:
+        raise ValueError("Attempted to add race to a non-existant season")
     
-    console = Console()
-    console.print(table)
+    season_data = track_data[season]
+    if track in season_data:
+        season_track_data = season_data[track]
+        season_track_data.append(pts)
+    else:
+        season_data[track] = [pts]
+    
+    dump_json(TRACK_DATA_PATH, track_data)
 
-def load_json(filename: str) -> dict:
-    with open(filename, "r") as f:
-        return json.load(f)
 
-        
-def main():
-    # first_season = get_first_season()
-    # current_season = get_current_season()
-    # all_seasons = fetch_seasons(*(s for s in range(first_season, current_season+1)))
-    # displayable = get_display_format(all_seasons)
-    # overall_avg, total_mogis = get_general_info(all_seasons)
-    # title = f"{overall_avg} average across {total_mogis} mogis"
-    # print_table(displayable, title=title)
+def revert_race(track: str, season_id: int = None):
+    season_id = get_current_season_id() if season_id is None else season_id
+    track_data = load_json(TRACK_DATA_PATH)
+    season = f"season_{season_id}"
+    if season not in track_data:
+        raise ValueError("Attempted to revert race from a non-existant season")
+    
+    season_data = track_data[season]
+    if track in season_data:
+        season_track_data = season_data[track]
+        if len(season_track_data) > 0:
+            season_track_data.pop()
+        if len(season_track_data) == 0:
+            season_data.pop(track)
 
-    track_data = load_json("track_data.json")
-    print(track_data)
+    dump_json(TRACK_DATA_PATH, track_data)
 
+
+def new_season(new_id: int) -> None:
+    json = load_json(TRACK_DATA_PATH)
+    new_entry = f"season_{new_id}"
+    json[new_entry] = {}
+    dump_json(TRACK_DATA_PATH, json)
+    update_config("currentseason", str(new_id))
+
+
+def help():
+    data_array = [
+        ('add race', 'add a new race to the stats. Input track name + points (eg. "lc 7")'),
+        ('revert', 'reverts the last race you added to a given track. Input as track name + revert (eg. "mc3 revert")'),
+        ('average', 'shows various information about your performance on each track (average score, standard deviation)'),
+        ('last x', '"x" is replaced by a number, which shows your stats across the last x amount of races'),
+        ('first x', '"x" is replaced by a number, which shows your stats across the first x amount of races'),
+        ('current', 'shows your stats for the current season'),
+        ('new season', 'changes to a new season'),
+        ('season x', '"x" is replaced by a number, which shows your stats from a given season'),
+        ('track data', 'shows all data for a given track in chronological order (eg. "dks data")'),
+        ('dist', 'shows hows often each track is picked per mogi')
+    ]
+    display_table(data_array, "Command", "Description", styles=["magenta", "white"], justify=["center", "left"])
+
+
+def print_dist_table(df: dict, title: str = None) -> None:
+    _, total_mogis = get_macro_info(df)
+    data_array = [
+        (
+            track, # Track name
+            "{:.0%}".format(len(scores)/total_mogis), # percentage
+            round(average(scores), 1), # Average score
+        )
+        for track, scores in df.items()
+    ]
+    data_array.sort(key = lambda x: float(x[1].strip("%")) / 100.0, reverse=True)
+    title = f"Track distribution across {total_mogis} mogis" if title is None else title
+    display_table(data_array, "track", "%", "avg", title=title)
+
+
+def print_avg_table(df: dict, title:str = None) -> None:
+    data_array = [
+        (
+            track, # Track name
+            round(average(scores), 1), # Average score
+            len(scores), # Number of races played
+            round(stdev(scores), 1) # standard deviation
+        ) 
+        for track, scores in df.items()
+    ]
+    data_array.sort(key = lambda x : x[1], reverse=True)
+
+    if title is None:
+        overall_avg, total_mogis = get_macro_info(df)
+        title = f"{total_mogis} mogis with a {overall_avg} average"
+    
+    display_table(data_array, "track", "avg", "races", "STDev", title=title)
 
 
 
 if __name__ == "__main__":
-    main()
-    from time import sleep
-    #sleep(10)
+    FIRST_SEASON = get_first_season_id()
+    CURRENT_SEASON = get_current_season_id()
+
+    while True:
+        query = input("Query: ").lower()
+        query_type = get_query_type(query)
+        args = query.split()
+
+        if query_type is None:
+            print('Invalid query; check your formatting. Consider using "help".')
+            continue
+
+        elif query_type == QUERY_TYPE.ADD:
+            track, pts = args[0], int(args[1])
+            if pts < 0 or pts > 15:
+                print("Invalid number of points.")
+                continue
+            else:
+                add_race(track, pts)
+        
+
+        elif query_type == QUERY_TYPE.REVERT:
+            track = args[0]
+            revert_race(track)
+
+
+        elif query_type == QUERY_TYPE.AVERAGE:
+            df = get_seasons(*(s_id for s_id in range(FIRST_SEASON, CURRENT_SEASON+1)))
+            print_avg_table(df)
+
+
+        elif query_type == QUERY_TYPE.DIST:
+            df = get_seasons(*(s_id for s_id in range(FIRST_SEASON, CURRENT_SEASON+1)))
+            print_dist_table(df)
+
+
+        elif query_type == QUERY_TYPE.LAST_X:
+            df = get_seasons(*(s_id for s_id in range(FIRST_SEASON, CURRENT_SEASON+1)))
+            amount = int(args[1])
+            trim_to_subset(df, amount, side="back")
+            print_avg_table(df, title=f"Average across last {amount} races for each track")
+
+
+        elif query_type == QUERY_TYPE.FIRST_X:
+            df = get_seasons(*(s_id for s_id in range(FIRST_SEASON, CURRENT_SEASON+1)))
+            amount = int(args[1])
+            trim_to_subset(df, amount, side="front")
+            print_avg_table(df, title=f"Average across first {amount} for each track")
+        
+
+        elif query_type == QUERY_TYPE.SEASON_X:
+            if query == "cur" or query == "current":
+                season_id = CURRENT_SEASON
+            else:
+                season_id = re.match(r"([a-z ]+)([0-9]+)", query).groups()[1] # Splits the string into string value at index 0 and numeric value at index 1
+            df = get_seasons(season_id)
+            print_avg_table(df)
+
+
+        elif query_type == QUERY_TYPE.NEW_SEASON:
+            print(f"Are you sure you want to change from season {CURRENT_SEASON} to season {CURRENT_SEASON+1}?")
+            confirmation = input('Type "yes" to confirm:')
+            if confirmation == "yes":
+                new_season(CURRENT_SEASON+1)
+                print("New season has started!")
+            else:
+                print("Did not change to new season.")
+                continue
+
+
+        elif query_type == QUERY_TYPE.TRACK_DATA:
+            track = args[0]
+            df = get_seasons(*(s_id for s_id in range(FIRST_SEASON, CURRENT_SEASON+1)))
+            print(df[track])
+
+        
+        elif query_type == QUERY_TYPE.HELP:
+            help()
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import math
 import json
+from configparser import ConfigParser
 
 def variance(data, ddof=0):
     n = len(data)
@@ -16,3 +17,25 @@ def average(lst: list) -> float:
 def load_json(filename: str) -> dict:
     with open(filename, "r") as f:
         return json.load(f)
+    
+def dump_json(filename: str, data) -> None:
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=4)
+    
+def get_current_season_id() -> int:
+    config = ConfigParser()
+    config.read("settings.ini")
+    return int(config["GENERAL"]["currentseason"])
+
+def get_first_season_id() -> int:
+    config = ConfigParser()
+    config.read("settings.ini")
+    return int(config["GENERAL"]["firstseason"])
+
+def update_config(parameter: str, value: str):
+    config = ConfigParser()
+    config.read("settings.ini")
+    config.set("GENERAL", parameter, value)
+
+    with open("settings.ini", "w") as configfile:
+        config.write(configfile)
